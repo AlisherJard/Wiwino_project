1b1b251343b9704a07ff3e3ffd6adc18341dc44f
ChristianValery <c.nguembou@gmail.com>
2024-08-23 16:03:56 +0200

    Add comments to the script_01.py file.

--- scripts/script_01.py
+++ scripts/script_01.py
@@ -2,6 +2,7 @@
 import csv
 import sqlite3
 
+# Connect to the database
 connexion = sqlite3.connect("db/vivino.db")
 cursor = connexion.cursor()
 
@@ -26,11 +27,14 @@ cursor.execute("""
         LEFT JOIN vintage_toplists_rankings AS vtr ON v.id = vtr.vintage_id;
 """)
 
+# Create a list of lists, data, where the first list contains the column's names.
 data = [[item[0] for item in cursor.description]]
 
+# Add the result of the query to the data list row by row.
 for item in cursor.fetchall():
     data.append(list(item))
 
+# Create the csv file.
 with open('CSVs/query_01.csv', 'w', newline='', encoding='utf-8') as file:
     writer = csv.writer(file)
     writer.writerows(data)
