11d8b87796657aa4b0a955060ea6194cf79b2aed
AlisherJard <alisherjard@gmail.com>
2024-08-26 16:36:23 +0200

    update

--- db/question6(wines)_query.sql
+++ db/question6(wines)_query.sql
@@ -1,15 +1,25 @@
+WITH first_table AS (
+    SELECT
+        wines.name AS wine_name,
+        countries.name AS country_name,
+        ratings_average,
+        ratings_count,
+        (ratings_average * ratings_count) AS sum_average
+    FROM
+        wines
+    JOIN
+        regions ON wines.region_id = regions.id
+    JOIN
+        countries ON regions.country_code = countries.code
+)
+
 SELECT
-    countries.name AS country_name,
-    ROUND(SUM(wines.ratings_average)/COUNT(countries.name), 4) AS average_rating,
-    ROUND(AVG(wines.ratings_count),0) AS avg_ratings_count,
-    COUNT(wines.name) AS wines_per_country
+    country_name,
+    SUM(ratings_count) AS total_ratings_count,
+    SUM(sum_average) AS total_sum_average,
+    ROUND(SUM(sum_average) / SUM(ratings_count),2) AS weighted_average_rating
 FROM
-    wines
-JOIN
-    regions ON wines.region_id = regions.id
-JOIN
-    countries ON regions.country_code = countries.code
+    first_table
 GROUP BY
-    countries.name
-ORDER BY
-    average_rating DESC;
+    country_name
+ORDER BY weighted_average_rating DESC;
