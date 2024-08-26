34b1df5ec8c22a5113e363b01ac2bfb5b1aa8b23
ChristianValery <c.nguembou@gmail.com>
2024-08-26 10:23:21 +0200

    Updating the files script_05.py and query_05.csv.

--- scripts/script_05.py
+++ scripts/script_05.py
@@ -6,41 +6,60 @@ import sqlite3
 connexion = sqlite3.connect("db/vivino.db")
 cursor = connexion.cursor()
 
+
+# A query to get the three most used grapes in the world
 cursor.execute("""
-        SELECT
-               w.name AS wine_name,
-               s.grape_name AS grape_name,
-               w.ratings_average AS wine_ratings_average,
-               w.ratings_count AS wine_ratings_count
-        FROM wines AS w
-        LEFT JOIN regions AS r ON w.region_id = r.id
-        LEFT JOIN countries AS c ON r.country_code = c.code
-        INNER JOIN
-            -- A subquery to get the three most used grapes in the world
-            (SELECT
-                g.name AS grape_name,
-                COUNT(DISTINCT c.name) AS countries_count,
-                c.code AS country_code
-            FROM most_used_grapes_per_country AS m
-            LEFT JOIN grapes AS g
-                ON m.grape_id = g.id
-            LEFT JOIN countries AS c
-                ON m.country_code = c.code
-            GROUP BY g.name
-            ORDER BY countries_count DESC
-            LIMIT 3) AS s
-                ON c.code = s.country_code
-        ORDER BY wine_ratings_average DESC, wine_ratings_count DESC;
+    SELECT
+        g.name AS grape_name,
+        COUNT(DISTINCT m.country_code) AS countries_count
+    FROM most_used_grapes_per_country AS m
+    LEFT JOIN grapes AS g
+        ON m.grape_id = g.id
+    GROUP BY g.name
+    ORDER BY countries_count DESC
+    LIMIT 3
 """)
 
 # Create a list of lists, data, where the first list contains the column's names.
-data = [[item[0] for item in cursor.description]]
+data_01 = [[item[0] for item in cursor.description]]
 
 # Add the result of the query to the data list row by row.
 for item in cursor.fetchall():
-    data.append(list(item))
+    data_01.append(list(item))
+
+# Create the csv file.
+with open('CSVs/top3_grapes.csv', 'w', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    writer.writerows(data_01)
+
+
+# Query the wine name containing one of the following string:
+# 'Cabernet Sauvignon', 'Merlot', and 'Chardonnay'
+# whose are the top three grapes used in the most countries
+cursor.execute("""
+    SELECT
+        name AS wine_name,
+        CASE WHEN name LIKE '%Cabernet Sauvignon%' THEN 'Cabernet Sauvignon'
+             WHEN name LIKE '%Merlot%' THEN 'Merlot'
+             WHEN name LIKE '%Chardonnay%' THEN 'Chardonnay'
+             END AS grape_name,
+             ratings_average,
+             ratings_count
+    FROM wines
+    WHERE name LIKE '%Cabernet Sauvignon%'
+        OR name LIKE '%Merlot%'
+        OR name LIKE '%Chardonnay%'
+    ORDER BY ratings_average DESC, ratings_count DESC;
+""")
+
+# Create a list of lists, data, where the first list contains the column's names.
+data_02 = [[item[0] for item in cursor.description]]
+
+# Add the result of the query to the data list row by row.
+for item in cursor.fetchall():
+    data_02.append(list(item))
 
 # Create the csv file.
 with open('CSVs/query_05.csv', 'w', newline='', encoding='utf-8') as file:
     writer = csv.writer(file)
-    writer.writerows(data)
+    writer.writerows(data_02)
