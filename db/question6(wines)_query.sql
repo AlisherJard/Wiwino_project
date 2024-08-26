a523b9290ff9e754ead050c836a6bc626bd921b7
AlisherJard <alisherjard@gmail.com>
2024-08-26 11:58:14 +0200

    update

--- db/question6(wines)_query.sql
+++ db/question6(wines)_query.sql
@@ -1,6 +1,8 @@
 SELECT
     countries.name AS country_name,
-    ROUND(SUM(wines.ratings_average)/COUNT(countries.name), 4) AS average_rating
+    ROUND(SUM(wines.ratings_average)/COUNT(countries.name), 4) AS average_rating,
+    ROUND(AVG(wines.ratings_count),0) AS avg_ratings_count,
+    COUNT(wines.name) AS wines_per_country
 FROM
     wines
 JOIN
